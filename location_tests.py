fb04956d545894655d92bcbf9b4abd1faaab75bf
Ryan <rghunter@calpoly.edu>
2019-04-08 15:11:54 -0700

    wrote location tests

--- location_tests.py
+++ location_tests.py
@@ -6,7 +6,11 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
+        loc1 = Location("here", 23.4, 23.5)
+        self.assertEqual(repr(loc1), "Location('here', 23.4, 23.5)") 
+        loc2 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(loc1 == loc2, False)
+        self.assertEqual(loc == loc2, True) 
     # Add more tests!
 
 if __name__ == "__main__":
